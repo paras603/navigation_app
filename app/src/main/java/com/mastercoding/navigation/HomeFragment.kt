6a4c6677dbbde73153703571445becddbc060d3b
paras603 <parasmanirai1@gmail.com>
2023-08-22 11:10:39 +0545

    used navigation to navigate other fragments

---
+++ app/src/main/java/com/mastercoding/navigation/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.mastercoding.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.mastercoding.navigation.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        //handling the click event button
+        binding.btnSubmit.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/mastercoding/navigation/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.mastercoding.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.mastercoding.navigation.databinding.FragmentHomeBinding
+import com.mastercoding.navigation.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second, container, false)
+        return binding.root
+    }
+
+}
